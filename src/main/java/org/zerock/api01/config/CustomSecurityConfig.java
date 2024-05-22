bb9491e5d35f0dee70872b66a408eb022bb2fe77
gurrms95 <gurrms95@gmail.com>
2024-05-22 17:56:36 +0900

    Token생성~

--- src/main/java/org/zerock/api01/config/CustomSecurityConfig.java
+++ src/main/java/org/zerock/api01/config/CustomSecurityConfig.java
@@ -6,6 +6,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,6 +17,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.zerock.api01.security.APIUserDetailService;
+import org.zerock.api01.security.filter.APILoginFilter;
 
 @Configuration
 @Log4j2
@@ -22,6 +27,8 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class CustomSecurityConfig {
+    
+    private final APIUserDetailService apiUserDetailService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -42,13 +49,37 @@ public class CustomSecurityConfig {
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception{
         log.info("------------------Configure-------------------");
 
+        //AuthenticationManagerBuilder설정
+        AuthenticationManagerBuilder authenticationManagerBuilder = 
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        
+        authenticationManagerBuilder
+                .userDetailsService(apiUserDetailService)
+                .passwordEncoder(passwordEncoder());
+        
+        //Get AuthenticationManager
+        AuthenticationManager authenticationManager =
+                authenticationManagerBuilder.build();
+        
+        //반드시 필요
+        http.authenticationManager(authenticationManager);
+
+        //APILoginFilter
+        APILoginFilter apiLoginFilter = new APILoginFilter("/generateToken");
+        apiLoginFilter.setAuthenticationManager(authenticationManager);
+        
+        //APILoginFilter의 위치조정
+        http.addFilterBefore(apiLoginFilter, UsernamePasswordAuthenticationFilter.class);
+
         //CSRF 토큰 비활성화
 //        http.csrf().disable();
         http.csrf(AbstractHttpConfigurer::disable);
+
         //세션을 사용하지 않음
 //        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.sessionManagement((sessionManagement)->
                 sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         return http.build();
     }
 }
